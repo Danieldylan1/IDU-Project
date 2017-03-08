352933325b8a975c7a041a3ab8207c6b86a1ca29
Danieldylan1 <shadowbox826@gmail.com>
2017-03-07 19:44:21 -0500

    data files added

---
+++ README.md
@@ -0,0 +1,5 @@
+Python IDU project for Computer Science
+Reads file of data and graphs
+Dependencies: Anaconda, Python
+
+Created by Jason Liu and Robert Bao
---
+++ src/main.py
@@ -0,0 +1,8 @@
+#Dependencies
+import numpy as np
+import matplotlib.pyplot as plt
+
+months1 = ["Jan 1999", "Feb 1999", "Mar 1999", "Apr 1999", "May 1999", "Jun 1999", "Jul 1999", "Aug 1999", "Sep 1999", "Oct 1999", "Nov 1999", "Dec 1999"] #Split months into 2 lists to get past limit
+months2 = ["Jan 2000", "Feb 2000", "Mar 2000", "Apr 2000", "May 2000", "Jun 2000", "Jul 2000", "Aug 2000", "Sep 2000", "Oct 2000", "Nov 2000", "Dec 2000", "Jan 2001"]
+months = months1 + months2 #concatenate lists
+tropoData = [26.586, 31.765, 33.101, 33.710, 45.250, 55.208, 55.551, 48.713, 41.232, 37.143, 31.492, 23.823, 27.615, 32.059, 36.199, 38.928, 52.081, 53.245, 51.406, 43.303, 40.135, 31.772, 22.736, 28.187, 21]
