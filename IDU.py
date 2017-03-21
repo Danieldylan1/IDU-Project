521af0b6d1a5a63db2ae7e5c6dcd872f497f45e5
Jason Liu <shadowbox826@gmail.com>
2017-03-21 12:58:37 +0000

    DONE >> Choice of graphs   INDEV >> Analysis of data

--- IDU.py
+++ IDU.py
@@ -3,9 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Open Files
-tfile = open("data/tropo.txt", 'r')
-sfile = open("data/surftemp.txt", 'r')
-
+tfile = open('tropo.txt', 'r')
+sfile = open('surftemp.txt', 'r')
 #Procedures
 def remove_empty(l):
     """
@@ -21,12 +20,12 @@ def remove_empty(l):
 #Get Dates
 dates = []
 lines = tfile.read().splitlines() #Get lines from file
+tfile.seek(0)
 i = 0
 while i < len(lines): #Remove empty elements
     if lines[i] == '' or lines[i] == ' ':
         del lines[i]
     i = i + 1
-lines[len(lines) - 2] = lines[len(lines) - 2] + lines[len(lines) - 1] #Concatenate last elements
 del lines[-1] #Delete last element
 for i in lines:
     i.split(' ')
@@ -37,22 +36,37 @@ tdata = []
 sdata = []
 tlines = tfile.read().splitlines()
 slines = sfile.read().splitlines()
+tdata.append(3.414)
 for i in tlines:
     i.split(' ')
     tdata.append(i[9:])
 remove_empty(tdata)
 tdata = [float(x) for x in tdata]
-for i in slines:
-    i.split(' ')
-    sdata.append(i[4:])
+for j in slines:
+    j.split(' ')
+    sdata.append(j[9:])
 remove_empty(sdata)
 sdata = [float(x) for x in sdata]
 
 #Graph Data
 #Plot surface temperatures against dates
-plt.plot(sdata)
-plt.title("Surface Temperature at ")
-plt.xlabel("Months (JAN 1999 - JAN 2001)")
-plt.xticks(np.arange(25), dates)
-plt.ylabel("Surface Temperature (C)")
-plt.show()
+chooseGraph = ""
+while chooseGraph != "quit":
+    chooseGraph = input("Please choose a graph or analysis to display (Surface Temperature against Months (smonth), Tropospheric Ozone against Months (tmonth), Surface Temperature against Tropospheric Ozone (surfozone), Analysis (analysis)) ")
+    if chooseGraph == "smonth":
+        plt.plot(sdata)
+        plt.title("Surface Temperature at 35 N, 85 W")
+        plt.xlabel("Months (JAN 1999 - JAN 2001)")
+        plt.ylabel("Surface Temperature (C)")
+        plt.xticks(np.arange((len(dates)) + 1), dates)
+        plt.show()
+    elif chooseGraph == "tmonth":
+        plt.plot(tdata)
+        plt.title("Tropospheric Ozone at 35 N, 85 W")
+        plt.xlabel("Months (JAN 1999 - JAN 2001)")
+        plt.ylabel("Tropospheric Ozone (Dobson Units)")
+        plt.xticks(np.arange((len(dates)) + 1), dates)
+        plt.show()
+    elif chooseGraph == "surfozone":
+        plt.scatter(sdata, tdata)
+        plt.title("Comparison Between Surface Temperature and Tropospheric Ozone at 35 N, 85 W")
